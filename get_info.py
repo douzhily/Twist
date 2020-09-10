1215f29cbe6efffc8f9fcb323c2de2a0c4990b8f
doudou <doudou@doudoudeMacBook-Pro.local>
2020-09-10 14:48:54 +0800

    update get_pid function.

--- get_info.py
+++ get_info.py
@@ -9,14 +9,15 @@ devices = os.popen('adb devices').readlines()
 
 def get_pid():
 	package_name = raw_input('Please enter package name : ').split()[0]
+	print package_name
 	cmd = 'adb shell ps | grep ' + package_name
 	pidinfo = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE).stdout.readlines()
 	#print pidinfo
 	for pid_i in pidinfo:
-		pid_name = pid_i.split()[8]
+		pid_name = pid_i.split()[9]
 		if pid_name == package_name:
 			pid = pid_i.split()[1]
-			print 'PID of ' + package_name + ' is ' + str(pid)
+			print ('PID of ' + package_name + ' is ' + str(pid))
 			return int(pid)
 
 
@@ -29,11 +30,11 @@ def get_mem_info(pid, native_heap_data, dalvic_heap_data):
 		native_heap_id = str(mem_i).find('Native Heap')
 		dalvic_heap_id = str(mem_i).find('Dalvik Heap')
 		if native_heap_id > 0:
-			print mem_i
+			print (mem_i)
 			native_info = mem_i.split()[2]
 			native_heap_data.append(int(native_info))
 		elif dalvic_heap_id > 0:
-			print mem_i
+			print (mem_i)
 			dalvic_info = mem_i.split()[2]
 			dalvic_heap_data.append(int(dalvic_info))
 			break
@@ -42,9 +43,9 @@ def get_mem_info(pid, native_heap_data, dalvic_heap_data):
 def get_cpu_info(pid, cpu_data):
 
 	cmd = 'adb shell top -n 1 | grep ' + str(pid)
-	print cmd
+	print (cmd)
 	cpuinfo = subprocess.Popen(cmd, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE).stdout.readlines()
-	print cpuinfo
+	print (cpuinfo)
 	cpu_info = cpuinfo[0].split()[8]
 	cpu_data.append(float(cpu_info))
 
